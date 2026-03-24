1e494342c05d5eaae7ec333f892b22e094c725a7
Shinwell Hu <huxinwei@huawei.com>
2026-03-24 13:14:54 +0800

    chore: remove build artifacts, add target/ to .gitignore
    Remove rustBindings/aclnn-sys/target and rustBindings/ascendcl-sys/target
    from git tracking. Add **/target/ to .gitignore to prevent future commits
    of Rust build artifacts.

--- rustBindings/aclnn-sys/target/debug/build/aclnn-sys-dcbd464ee6fbd9ad/build_script_build-dcbd464ee6fbd9ad.d
+++
@@ -1,5 +0,0 @@
-/home/xinwei-hu/workspace/pypto_workspace/rustBindings/aclnn-sys/target/debug/build/aclnn-sys-dcbd464ee6fbd9ad/build_script_build-dcbd464ee6fbd9ad.d: build.rs
-
-/home/xinwei-hu/workspace/pypto_workspace/rustBindings/aclnn-sys/target/debug/build/aclnn-sys-dcbd464ee6fbd9ad/build_script_build-dcbd464ee6fbd9ad: build.rs
-
-build.rs:
--- rustBindings/ascendcl-sys/target/debug/build/ascendcl-sys-1c93c7c1acfd84b5/build_script_build-1c93c7c1acfd84b5.d
+++
@@ -1,5 +0,0 @@
-/home/xinwei-hu/workspace/pypto_workspace/rustBindings/ascendcl-sys/target/debug/build/ascendcl-sys-1c93c7c1acfd84b5/build_script_build-1c93c7c1acfd84b5.d: build.rs
-
-/home/xinwei-hu/workspace/pypto_workspace/rustBindings/ascendcl-sys/target/debug/build/ascendcl-sys-1c93c7c1acfd84b5/build_script_build-1c93c7c1acfd84b5: build.rs
-
-build.rs:
